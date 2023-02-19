6acc90ea587ab133ff107cc5f92c9ff81b3cee91
Jack Wayne <jack.wayne98@outlook.com>
2023-02-19 13:42:58 -0600

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int first_row, second_row;
+
+	for (first_row = 48; first_row <= 57; first_row++)
+	{
+		for (second_row = 49; second_row <= 57; second_row++)
+		{
+			if  (first_row != second_row && (second_row > first_row))
+			{
+				putchar(first_row);
+				putchar(second_row);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
