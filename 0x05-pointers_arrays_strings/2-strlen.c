53dfcf20b41358f38f1ef8bce4f2085fb40a1467
Jack Wayne <jack.wayne98@outlook.com>
2023-02-28 00:42:39 -0600

    push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - sends back the lenght of a string
+ * @s: is a pointer to the original string
+ * Description: Function to reurn the lenght of a string
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		x++;
+	}
+	return (x);
+}
