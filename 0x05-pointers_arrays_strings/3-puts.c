495d85f55f3b085130c9df904bea400fbdc69353
Jack Wayne <jack.wayne98@outlook.com>
2023-02-28 00:51:42 -0600

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - prints the string and adds a new line
+ * @str: receives a string by reference
+ * Description: Function to return the string and a new line
+ * Return: none
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
