00787518d5bebb6d31a60d37f8829d6028057eaf
Jack Wayne <jack.wayne98@outlook.com>
2023-02-27 23:42:46 -0600

    push

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * swwap - takes a pointer to an int as a parameter
- * and updated the value it points to 98
+ * swap_int - takes to integers and swaps their values
+ * @a: is converted to the value of b
+ * @b: is converted to the value of a
  * Description: changes the value by reference of a variable
- * @n: pointer to an int
  * Return: always 0
  */
 void swap_int(int *a, int *b)
 {
-        int temp = *a;
+	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
