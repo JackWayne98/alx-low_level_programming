425d9f2ba4bba81faccf318b4ebd87b3e115b5d6
Jack Wayne <jack.wayne98@outlook.com>
2023-02-28 08:18:34 -0600

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - prints in reverse
+ * @s: receives a string by reference and reverses it
+ * Description: Function to return the string and a new line
+ * Return: none
+ */
+void rev_string(char *s)
+{
+	int i, j, x = 0;
+	char *f = s;
+	char temp[100];
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		temp[x] = f[j];
+		printf("%c", temp[x]);
+		x++;
+	}
+	printf("\n");
+}
