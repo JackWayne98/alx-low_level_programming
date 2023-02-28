90a24a4acf36376d58e3bed5366ac5b6f442c891
Jack Wayne <jack.wayne98@outlook.com>
2023-02-27 22:12:34 -0600

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,6 +15,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void pri printf("A[0] %d\n", *a);nt_times_table(int n);
 
 #endif /* MAIN.H*/
