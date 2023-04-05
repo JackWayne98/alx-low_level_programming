7a541dc877b519c0698566b9e4b457099ae02c7d
Jack Wayne <jack.wayne98@outlook.com>
2023-04-05 07:57:48 -0600

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar(1, "_putchar", 8);
+	putchar(10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/READM.md
@@ -0,0 +1,2 @@
+This repository contains all the tasks related to the 0x02. C-Functions, nested loops
+
