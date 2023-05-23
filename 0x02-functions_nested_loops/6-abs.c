a0c399dd6e0d5a4b074f0c2a9e91d9899e753c5b
Jack Wayne <jack.wayne98@outlook.com>
2023-05-23 10:01:31 -0600

    returns the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * _absint - evaluates the alhabetic character
+ *
+ * Description: evaluates a character if it's c then return 1, otherwise 0
+ * @c: receives a lower or uppercase character
+ * Return: 1 if c is lowercase/upper, otherwise 0
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
