74a9d246ce7183b4834e55257d976d4e143382d5
Jack Wayne <jack.wayne98@outlook.com>
2023-05-23 07:12:39 -0600

    checks for lower case character if it's a c provides 1 as a response, otherwise 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * _islower - prints the alphabet in lowercase 10x
+ *
+ * Description: evaluates a character if it's c then return 1, otherwise 0
+ * @c: receives a character
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	if (c == 99)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
