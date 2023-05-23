db92df2ba543b756cbb68fd20af95f59c3bc3904
Jack Wayne <jack.wayne98@outlook.com>
2023-05-23 09:17:04 -0600

    Function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (putchar('+'), 1);
+		return (1, putchar('+'));
 	}
 	else if (n == 0)
 	{
-		return (putchar('0'), 0);
+		return (0, putchar('0'));
 	}
 	else
 	{
-		return (putchar('-'), 1);
+		return (1, putchar('-'));
 	}
 }
