e6df184d78f5c2a38d9b7f5cde90baef4e698409
Jack Wayne <jack.wayne98@outlook.com>
2023-05-24 14:49:05 -0600

    prints numbers from 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@
 void more_numbers(void)
 {
 	int i, j;
-	
+
 	for (j = 0; j < 9; j++)
 	{
 		for (i = 1; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-				putchar((i/10)+'0');
+				putchar((i / 10) + '0');
 			}
-			putchar((i%10)+'0');
+			putchar((i % 10) + '0');
 		}
 		putchar(10);
 	}
