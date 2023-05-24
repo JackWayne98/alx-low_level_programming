b1a9985520070897cd94ab72e6ee63d3316a392c
Jack Wayne <jack.wayne98@outlook.com>
2023-05-24 13:36:01 -0600

    Multiplies two integers and returns the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function
+ *
+ * Description: Multiplies two integers and returns the result of 'em
+ * @a: receives an integer
+ * @b: receives an integer
+ * Return: Multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
