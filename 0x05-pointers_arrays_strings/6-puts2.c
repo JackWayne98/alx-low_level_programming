ac4015857e9d4f6a3727efbc1342affc30deb802
Jack Wayne <jack.wayne98@outlook.com>
2023-05-29 17:38:54 -0600

    half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * _puts - prints the string and adds a new line
+ * puts2 - prints the string by omitting the one chararacter next to each other
  * @str: receives a string by reference
  * Description: Function to return the string and a new line
  * Return: none
  */
 void puts2(char *str)
 {
-	puts(str);
+	int i = 0;
+	int len;
+
+	for (len = 0; str[len] != 0; len++)
+	{
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar(10);
 }
