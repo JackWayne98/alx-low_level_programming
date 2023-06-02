8b9d7176b86df302de046cc6ce3f234262cb639a
Jack Wayne <jack.wayne98@outlook.com>
2023-06-01 18:48:18 -0600

    function that copies a string until N characters

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,11 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	for (i = 0; i <= len; i++)
 	{
-		dest[i] = src[i];
 		if (i == n)
 		{
 			break;
 		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
