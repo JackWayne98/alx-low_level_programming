ec4f1c7187abfdfd0845271a740f473a3445e0a4
Jack Wayne <jack.wayne98@outlook.com>
2023-06-01 18:44:30 -0600

    function that copies a string until N characters

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - Copies string pointed by src, including the terminating null byte
+ * @dest: receives an array
+ * @src: receives the size of the array
+ * @n: receives an integer
+ * Description: Function to return the string and a new line
+ * Return: none
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len;
+
+	for (len = 0; src[len] != 0; len++)
+	{
+	}
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+		if (i == n)
+		{
+			break;
+		}
+	}
+	return (dest);
+}
