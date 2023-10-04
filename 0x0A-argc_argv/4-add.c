cd0ddf5317707620c62e582cd17a0ff7cc9801fd
Jack Wayne <jack.wayne98@outlook.com>
2023-10-04 05:34:49 -0600

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Entry point
@@ -9,21 +10,32 @@
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i, mul;
+	int i, j, adds = 0;
 
 	if (argc > 1)
 	{
-		for (i = 2; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
+			char *arg = argv[i];
+
+			for (j = 0; arg[j] != '\0'; j++)
+			{
+				if (!isdigit(arg[j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			if (atoi(argv[i]) >= 0)
 			{
-				mul = atoi(argv[i - 1]) * atoi(argv[i]);
+				adds += atoi(argv[i]);
 			}
 		}
+		printf("%d\n", adds);
 	}
 	else
 	{
-		printf("Error\n");
+		printf("0\n");
 	}
 	return (0);
 }
