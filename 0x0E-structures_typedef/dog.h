a2e2f5adc5ef10ee86dfd32bb41bc200226b03ad
Jack Wayne <jack.wayne98@outlook.com>
2023-10-06 18:54:52 -0600

    Basic Struct to define a Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ ** struct dog - a dog's info struct
+ ** @name: First member of dog
+ ** @age: Second member of dog
+ ** @owner: Third member of dog
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
