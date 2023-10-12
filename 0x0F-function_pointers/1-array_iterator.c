7c8a2687d1d15c6f5b19b932b53d10675352398c
Jack Wayne <jack.wayne98@outlook.com>
2023-10-11 18:10:46 -0600

    A call back function to print an array through a pointer function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * array_iterator- prints an array as a call-back function
+ * @array: pointer to a char as an argument
+ * @size: the size of the array
+ * @action: pointer to a function as an argument
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
