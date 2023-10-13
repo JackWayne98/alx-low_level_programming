4d8afb6f3393d94f94a372be320a06550540bd3f
Jack Wayne <jack.wayne98@outlook.com>
2023-10-12 18:40:52 -0600

    function that prints numbers, followed by a new line.

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints arguments separated by commas
+ * @separator: string that represents a comma
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	va_start(args, n);
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else if (separator && i == 0)
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else
+		{
+			printf("%s %d", separator, va_arg(args, int));
+		}
+	}
+	va_end(args)i;
+}
