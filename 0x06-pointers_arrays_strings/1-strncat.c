805eca052230d6e91d8a4b73aa81bdf5b6fcbb16
Jack Wayne <jack.wayne98@outlook.com>
2024-02-03 23:36:30 -0600

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,24 +10,20 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int j = 0;
-	int len, len2;;
+	int len, len2;
 
 	for (len = 0; dest[len] != '\0'; len++)
 	{
 	}
-	for (len2 = 0; src[len2] != '\0'; len2++))
+	for (len2 = 0; src[len2] != '\0'; len2++)
 	{
-		if (len2 == n)
-		{
-			break;
-		}
 	}
 	while (src[j] != '\0')
 	{
 		dest[len] = src[j];
 		j++;
 		len++;
-		if (j == len2)
+		if (j == len2 || j == n)
 		{
 			break;
 		}
